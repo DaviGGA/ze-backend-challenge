23aa2cd9d0b2464de560facf21468ec3cbaf49d0
DaviGGA <davigregorio.a@hotmail.com>
2025-01-29 20:38:07 -0300

    Create partnet model & geojson types

---
+++ src/modules/partners/PartnerModel.ts
@@ -0,0 +1,57 @@
+import mongoose, { Model } from "mongoose";
+import { MultiPolygon } from "./@types/MultiPolygon";
+import { Point } from "./@types/Point";
+
+export type IPartner = {
+  tradingName: string,
+  ownerName: string,
+  document: string,
+  coverageArea: {
+    type: "MultiPolygon",
+    coordinates: MultiPolygon
+  },
+  address: {
+    type: "Point",
+    coordinates: Point
+  }
+} & Document
+
+const Schema = new mongoose.Schema<IPartner>(
+  {
+    tradingName: {
+      type: String,
+      required: true,
+    },
+    ownerName: {
+      type: String,
+      required: true,
+    },
+    document: {
+      type: String,
+      required: true,
+    },
+    coverageArea: {
+      type: {
+        type: String
+      },
+      coordinates: {
+        type: [[[[Number, Number]]]]
+      }
+    },
+    address: {
+      type: {
+        type: String
+      },
+      coordinates: {
+        type: [Number, Number]
+      }
+    }
+  },
+  {
+    collection: "Partner",
+    timestamps: true
+  }
+)
+
+export const Partner: Model<IPartner> = mongoose.model('Partner', Schema);
+
